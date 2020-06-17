fbe7c48d141c869d7e05705ba58f77394376f50b
Moazzam Adil Khan <moaadi3890@gmail.com>
2020-06-17 13:14:22 +0500

    Registration of Route with Attribute Macro

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{Responder,web,App,HttpServer,HttpResponse};
+use actix_web::{Responder,web,App,HttpServer,HttpResponse,get};
 
 async fn req_handler_1() -> impl Responder{ //creation of a request handler
     HttpResponse::Ok().body("Hello Outside World!")
@@ -12,6 +12,11 @@ async fn req_handler_3() -> impl Responder{ //Recieves 0 or more parameters from
     HttpResponse::Ok().body("Welcome!")  //impl From Request in .to())
 }
 
+#[get("/attrmacro")] //Another way to define paths with Macro's
+async fn req_handler_4() -> impl Responder {
+    HttpResponse::Ok().body("Using Attribute Macro") 
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()>{
     HttpServer::new(||{ //creation of new App instance.
@@ -20,6 +25,8 @@ async fn main() -> std::io::Result<()>{
             .route("/outside", web::get().to(req_handler_1))
             .route("/inside", web::get().to(req_handler_2))
             .route("/welcome", web::get().to(req_handler_3))
+            //Registration of route using Attribute Macro | Just another way
+            .service(req_handler_4)
     })
     .bind("127.0.0.1:8088")?
     .run()
